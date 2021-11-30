e9293287fd13c14c7d70cd417e7b383e19746434
Paweł Pepek <pepek@riskout.pl>
2021-11-30 20:03:44 +0100

    Include connected tables in the procedure idgeFinder.FindRidge

---
+++ TatraRidgesModel/Helpers/PointsConnectionWithDirection.cs
@@ -0,0 +1,9 @@
+﻿namespace TatraRidges.Model.Helpers
+{
+    public class PointsConnectionWithDirection
+    {
+        public PointsConnection PointsConnection { get; set; }
+        public bool ConsistDirection{ get; set; }
+
+    }
+}
--- TatraRidgesModel/Procedures/RidgeFinder.cs
+++ TatraRidgesModel/Procedures/RidgeFinder.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using TatraRidges.Model.Helpers;
 
 namespace TatraRidges.Model.Procedures;
 
@@ -12,24 +13,41 @@ public class RidgeFinder
         _context = context;
     }
 
-    public List<PointsConnection> FindRidge(int pointFromId, int pointToId)
+    public List<PointsConnectionWithDirection> FindRidge(int pointFromId, int pointToId)
     {
-        _ridges = _context.PointsConnections.Where(con => con.Ridge);
+        _ridges = _context.PointsConnections.Where(con => con.Ridge)
+                                            .Include(c => c.Routes)
+                                            .ThenInclude(r => r.RouteType)
+                                            .Include(c => c.Routes)
+                                            .ThenInclude(r => r.Difficulty)
+                                            .Include(c => c.Routes)
+                                            .ThenInclude(r => r.DifficultyDetail)
+                                            .Include(c => c.Routes)
+                                            .ThenInclude(c => c.DescriptionAdjectivePairs)
+                                            .Include(c => c.Routes)
+                                            .ThenInclude(r => r.DescriptionAdjectivePairs)
+                                            .ThenInclude(a => a.Adjective);
         return FindRidgeForPoints(pointFromId, pointToId);
     }
 
-    private List<PointsConnection> FindRidgeForPoints(int pointFromId, int pointToId)
+    private List<PointsConnectionWithDirection> FindRidgeForPoints(int pointFromId, int pointToId)
     {
         var ridgesFromStart = AllConnectionsFromPoint(pointFromId);
         if (!ridgesFromStart.Any())
         {
-            return new List<PointsConnection>();
+            return new List<PointsConnectionWithDirection>();
         }
-        var ridgeToPointTo = ridgesFromStart.FirstOrDefault(r => r.PointId1 == pointToId || r.PointId2 == pointToId);
+        var ridgeToPointTo = ridgesFromStart
+                    .FirstOrDefault(r => r.PointId1 == pointToId || r.PointId2 == pointToId);
 
         if (ridgeToPointTo != null)
         {
-            return new List<PointsConnection>() { ridgeToPointTo };
+            var findedConnection = new PointsConnectionWithDirection()
+            {
+                PointsConnection = ridgeToPointTo,
+                ConsistDirection = ridgeToPointTo.PointId1 == pointFromId
+            };
+            return new List<PointsConnectionWithDirection>() { findedConnection };
         }
         else
         {
@@ -39,19 +57,25 @@ public class RidgeFinder
                 var connectedRidges = FindRidgeForPoints(nextPointFrom, pointToId);
                 if (connectedRidges.Any())
                 {
-                    connectedRidges.Insert(0, r);
+                    var findedConnection = new PointsConnectionWithDirection()
+                    {
+                        PointsConnection = r,
+                        ConsistDirection = r.PointId1 == pointFromId
+                    };
+                    connectedRidges.Insert(0, findedConnection);
                     return connectedRidges;
                 }
             }
         }
-        return new List<PointsConnection>();
+        return new List<PointsConnectionWithDirection>();
     }
 
     private IList<PointsConnection> AllConnectionsFromPoint(int pointFromId)
     {
-        var ridgesFromStart = _ridges.Where(con => con.PointId1 == pointFromId || con.PointId2 == pointFromId);
+        var ridgesFromStart = _ridges.Where(con => con.PointId1 == pointFromId || con.PointId2 == pointFromId)
+                                     .ToList();
         _ridges = _ridges.Where(r => !ridgesFromStart.Contains(r));
-        return ridgesFromStart.ToList();
+        return ridgesFromStart;
     }
 
     private static int PointDifferentLike(PointsConnection connection, int notThisPointId)
