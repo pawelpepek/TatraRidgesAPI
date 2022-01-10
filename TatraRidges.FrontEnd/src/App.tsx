382db403e3b6c0d2dc8ca950d892e04c3e3b23e4
Paweł Pepek <pepek@riskout.pl>
2022-01-10 13:38:35 +0100

    FronteEnd - code cleaning

--- TatraRidges.FrontEnd/src/App.tsx
+++ TatraRidges.FrontEnd/src/App.tsx
@@ -1,7 +1,4 @@
 import "./App.css"
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
-import { Point, icon } from "leaflet"
-import { useEffect, useState } from "react"
 import RidgeMapContainer from "./components/ridgeMap/RidgeMapContainer"
 
 function App() {
