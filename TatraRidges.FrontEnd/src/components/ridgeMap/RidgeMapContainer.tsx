e988780b2066df2a43d1c80d2a1683df8b91d287
Paweł Pepek <pepek@riskout.pl>
2022-01-10 13:37:50 +0100

    FrontEnd - finding map position

--- TatraRidges.FrontEnd/src/components/ridgeMap/RidgeMapContainer.tsx
+++ TatraRidges.FrontEnd/src/components/ridgeMap/RidgeMapContainer.tsx
@@ -1,22 +1,55 @@
-import { MapContainer, MapContainerProps, Marker, Popup } from "react-leaflet"
+import { MapContainer, Polyline } from "react-leaflet"
 import { TileLayer } from "react-leaflet"
+import { latLng, Map } from "leaflet"
 
 import RidgesMarkersContainer from "./RidgesMarkersContainer"
 import { RidgeMapProps } from "./basics/types"
+import { useState, useCallback } from "react"
+import {useHistory} from 'react-router-dom'
+import { useParams} from "react-router-dom"
+import { NavigationParams } from "./basics/types"
+
 
 const RidgeMapContainer: React.FC<RidgeMapProps> = props => {
+	// const params=useParams<NavigationParams>()
+	// console.log(params)
+	const defaultPosition = latLng(49.179306, 20.088444)
+	const defaultZoom = 14
+
+	const [[position, zoom], setPosition] = useState([
+		defaultPosition,
+		defaultZoom,
+	])
+
+	let map: Map
+
+	const onChangeMap = useCallback(() => {
+		setPosition([map.getCenter(), map.getZoom()])
+		console.log(position, zoom)
+	}, [])
+
+	const OnMapCreated = useCallback((m: Map) => {
+		map = m
+		setPosition([map.getCenter(), map.getZoom()])
+		map.on("moveend", onChangeMap)
+		map.on("zoomend", onChangeMap)
+		console.log(position)
+		// history.replace('/')
+	}, [])
+
 	return (
 		<MapContainer
-			center={[49.179306, 20.088444]}
-			zoom={14}
+			center={defaultPosition}
+			zoom={defaultZoom}
 			className={props.className}
 			id={props.id}
-			scrollWheelZoom={true}>
+			scrollWheelZoom={true}
+			whenCreated={OnMapCreated}>
 			<TileLayer
 				attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 				url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
 			/>
-			<RidgesMarkersContainer/>
+			<RidgesMarkersContainer />
 		</MapContainer>
 	)
 }
--- TatraRidges.FrontEnd/src/components/ridgeMap/basics/types.ts
+++ TatraRidges.FrontEnd/src/components/ridgeMap/basics/types.ts
@@ -1,6 +1,9 @@
 export interface RidgeMapProps {
 	id?: string
 	className?: string
+    lat?:number
+    log?:number
+    zoom?:number
 }
 
 export interface MountainPoint {
@@ -14,3 +17,24 @@ export interface MountainPoint {
 export interface MountainPoints{
     points:MountainPoint[]
 }
+
+export interface ConnectionData{
+    id:number
+    pointId1:number
+    pointId2:number
+}
+
+export interface PositionZoomInfo{
+    lat:number
+    log:number
+    zoom:number
+}
+
+export interface ConnectionLineProps{
+    id:number
+    point1:MountainPoint
+    point2:MountainPoint
+    color?: string | undefined;
+    onChangePositionZoom?(info:PositionZoomInfo):void
+}
+
