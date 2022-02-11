7fe01836a3e545817aa8528e365e61f76a11aef3
Paweł Pepek <pepek@riskout.pl>
2022-02-11 21:13:36 +0100

    Startup integration tests

---
+++ TatraRidgesAPI.IntegrationTests/StartupTests.cs
@@ -0,0 +1,46 @@
+﻿using Xunit;
+using Microsoft.AspNetCore.Mvc.Testing;
+using FluentAssertions;
+using System.Linq;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System;
+
+namespace TatraRidgesAPI.IntegrationTests
+{
+    public class StartupTests : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly List<Type> _controllerTypes;
+        private readonly WebApplicationFactory<Startup> _factory;
+        public StartupTests(WebApplicationFactory<Startup> factory)
+        {
+            _controllerTypes = typeof(Startup)
+                .Assembly
+                .GetTypes()
+                .Where(c => c.IsSubclassOf(typeof(ControllerBase)))
+                .ToList();
+
+            _factory = factory.WithWebHostBuilder(builder =>
+            {
+                builder.ConfigureServices(services =>
+                {
+                    _controllerTypes.ForEach(c => services.AddScoped(c));
+                });
+            });
+        }
+        [Fact]
+        public void ConfigureServices_ForControllers_RegistersAllDependencies()
+        {
+            var scopeFactory = _factory.Services.GetService<IServiceScopeFactory>();
+            using var scope = scopeFactory.CreateScope();
+
+            //assert
+            _controllerTypes.ForEach(t =>
+            {
+                var controller = scope.ServiceProvider.GetServices(t);
+                controller.Should().NotBeNull();
+            });
+        }
+    }
+}
