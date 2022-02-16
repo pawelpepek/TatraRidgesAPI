b02671493e9839854588ce335364a623ffe9b6e8
Paweł Pepek <pepek@riskout.pl>
2022-02-16 18:23:23 +0100

    Styling left panel points, the navigation and the header

--- TatraRidges.FrontEnd/src/components/left-panel/MainFunctions.tsx
+++ TatraRidges.FrontEnd/src/components/left-panel/MainFunctions.tsx
@@ -6,22 +6,19 @@ import FindPanel from "./find/FindPanel"
 import classes from "./MainFunctions.module.css"
 import NavigationPanel from "./NavigationPanel"
 import AuthForm from "./authorization/AuthForm"
-import UserPanel from "./UserPanel"
 
 const MainFunctions: React.FC = () => {
 	const visiblePanel = useSelector((state: StoreType) => state.ui.visiblePanel)
 
 	return (
 		<div className={classes.functions}>
-			<header>
+			<header className={classes.header}>
 				<h1>Granie Tatr Wysokich</h1>
 			</header>
 			<NavigationPanel className={classes.navigation} />
-			<div className={classes.panel}>
 				{visiblePanel === "admin" && <AdminPanel />}
 				{visiblePanel === "find" && <FindPanel />}
 				{visiblePanel === "login" && <AuthForm />}
-			</div>
 		</div>
 	)
 }
