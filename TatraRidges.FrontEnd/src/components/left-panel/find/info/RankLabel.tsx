e9a51e31f60a1a143be21394927b48fdbd98dea1
Paweł Pepek <pepek@riskout.pl>
2022-02-17 19:20:04 +0100

    Calculating color from value for RankLabel

--- TatraRidges.FrontEnd/src/components/left-panel/find/info/RankLabel.tsx
+++ TatraRidges.FrontEnd/src/components/left-panel/find/info/RankLabel.tsx
@@ -3,8 +3,21 @@ import classes from "./RankLabel.module.css"
 const RankLabel: React.FC<{ rank: number }> = props => {
 	const rank = "" + props.rank
 
+	const rangeFromNumber = (value: number) => {
+		const maxRank = 15
+		var range = Math.min(value, maxRank)
+		return Math.round((255 * (maxRank - range)) / maxRank)
+	}
+
+	const redValue = props.rank <= 0 ? 255 : rangeFromNumber(props.rank)
+	const greenValue = props.rank >= 0 ? 255 : rangeFromNumber(-props.rank)
+	const blueValue = rangeFromNumber(Math.abs(props.rank))
+
+	const backgroundColor = `rgb(${redValue},${greenValue},${blueValue})`
+	const sectionStyle = { backgroundColor: backgroundColor }
+
 	return (
-		<section className={classes.rank}>
+		<section className={classes.rank} style={sectionStyle}>
 			<p>{rank}</p>
 		</section>
 	)
