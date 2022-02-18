1029157f53951a4a8bb04a360254170d7fc28832
Paweł Pepek <pepek@riskout.pl>
2022-02-19 00:49:38 +0100

    DifficultyLabel code cleaning

--- TatraRidges.FrontEnd/src/components/left-panel/info/DifficultyLabel.tsx
+++ TatraRidges.FrontEnd/src/components/left-panel/info/DifficultyLabel.tsx
@@ -1,8 +1,12 @@
+import Tooltip from "../../ui/Tooltip"
 import classes from "./DifficultyLabel.module.css"
 
 const DifficultyLabel: React.FC<{ text: string; small?: boolean }> = props => {
-	const className = props.small ? classes.difficulty : classes["difficulty-max"]
+	const className = `${classes.difficulty} ${
+		!props.small && classes["difficulty-max"]
+	}`
 
+	const text = (props.small ? "Średni" : "Maksymalny") + " poziom trudności"
 	return <span className={className}>{props.text}</span>
 }
 
